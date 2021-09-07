1d57a8e62bdab7fbbfc3635e2d4c58f927fb0715
LJH95224 <991538766@qq.com>
2021-09-07 20:37:30 +0800

    tmp

---
+++ src/store/index.js
@@ -0,0 +1,47 @@
+/*
+ * @Description:
+ * @Date: 2021-09-07 20:13:46
+ * @FilePath: \cssFramework\src\store\index.js
+ */
+import { createStore } from 'vuex'
+import { login } from '@/api/me'
+import createPersistedState from 'vuex-persistedstate'
+
+export const store = createStore({
+  plugins: [createPersistedState()],
+  state: {
+    account: '',
+    avatar: '',
+    token: ''
+  },
+  mutations: {
+    setUser (state, user) {
+      state.account = user.account
+      state.avatar = user.avatar
+      state.token = user.token
+    }
+  },
+  actions: {
+    login ({ commit }, params) {
+      return new Promise((resolve, reject) => {
+        login(params)
+          .then(res => {
+            commit('setUser', res.data)
+            resolve()
+          })
+          .catch(error => {
+            reject(error)
+          })
+      })
+    },
+
+    logout ({ commit }) {
+      commit('setUser', {
+        account: '',
+        avatar: '',
+        token: ''
+      })
+    }
+  },
+  modules: {}
+})
