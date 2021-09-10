291504dc3e2411b4e8fe6cd3a50f7a24b63dd540
LJH95224 <991538766@qq.com>
2021-09-10 20:52:17 +0800

    'init'

--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import { store } from './store'
 import 'vant/lib/toast/style'
 import '@/mock' // mock数据
+import './style/gobal.scss'
 createApp(App).use(store).use(router).mount('#app')
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,11 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.about {
+  width: 400px;
+  height: 400px;
+  @include box-center(center, center)
+}
+</style>
